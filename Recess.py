fcc47868206125384af7174fdc40b02587be84f0
morganestes <morgan.estes@gmail.com>
2012-04-24 22:26:18 -0500

    Changed formatting using sublime linter tools.

--- Recess.py
+++ Recess.py
@@ -1,8 +1,9 @@
-import sublime, sublime_plugin
+import sublime_plugin
+
 
 class RecessCommand(sublime_plugin.WindowCommand):
-  def run(self):
-    self.window.run_command('set_build_system', {
-      'file': 'Packages/Recess/Recess.sublime-build'
-    })
-    self.window.run_command('build')
+    def run(self):
+        self.window.run_command('set_build_system', {
+          'file': 'Packages/Recess/Recess.sublime-build'
+        })
+        self.window.run_command('build')
